4d7476995824d52ca7552d66b46c6ec65779f45c
Sage M <sage@divgo.com>
2017-03-03 13:49:15 -0500

    Create Site & Hosts Entry based on TFS BranchName
    Powershell script, which when placed in the root of a branch in TFS, can be used to quickly configure a local developers machine. The script will attempt to derive the name of the TFS Branch from the Workspace mapping. If it is able to derive a Branch Name, it will create a Website and AppPool in IIS as well as an entry in the users HOSTS file. 
    
    Given a Branch Name of "Project1" for example, 
      - an AppPool called "Project1" will be created.
      - a WebSite named "Project1" will be created.
      - The WebSite "Project1" will have a binding "project1.localhost.com"
      - a Loopback entry in the HOSTS file will be created for "project1.localhost.com"
    
    Script uses the Carbon PS Module, which will be installed if it is not present.

---
+++ SetupLocal.ps1
@@ -0,0 +1,57 @@
+﻿Add-Type -Path "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Microsoft.TeamFoundation.VersionControl.Client.dll";
+
+# Install-Module PSHosts
+if (Get-Module -ListAvailable -Name Carbon) {
+    Write-Host "Carbon Module exists"
+} else {
+    Write-Host "Carbon Module does not exist"
+    Install-Module -Name 'Carbon' # -AllowClobber
+}
+
+Import-Module Carbon
+
+if ([Environment]::GetEnvironmentVariable("CredibleLocal") -eq $null) {
+    Write-Host "Creating CredibleLocal Environment Variable With Default Value";
+    $WebRootFolder = "C:\inetpub\wwwroot";
+    [Environment]::SetEnvironmentVariable("CredibleLocal", $WebRootFolder, "User")
+} else {
+    $WebRootFolder = [Environment]::GetEnvironmentVariable("CredibleLocal");
+}
+
+Write-Host $WebRootFolder;
+
+function ConfigureLocalSite($SiteName) {
+
+    $SiteDomain = $SiteName + ".localhost.com";
+    $AppPoolName = $SiteName.replace(".", "-"); # Site Modules names would have a period in it; eg: login.branchname, ext.branchname
+    $SiteDirectory = $WebRootFolder + "\" + $SiteName.replace(".", "-");
+
+    Write-Host "Creating HOSTS entry for: " $SiteDomain;
+    Set-HostsEntry -IPAddress 127.0.0.1 -HostName $SiteDomain;
+
+    Write-Host "Configuring IIS Website for Branch: " $SiteName;
+    If ( -Not (Test-Path -PathType Container -Path $SiteDirectory) ) {
+        Write-Host "Creating Target Website Folder: "+$SiteDirectory;
+        New-Item -ItemType Directory -Force -Path $SiteDirectory;
+    }
+    Install-IisAppPool -Name $AppPoolName;
+    $Binding = "http:/*:80:" + $SiteDomain;
+    Install-IisWebsite -Name $AppPoolName -PhysicalPath $SiteDirectory -AppPoolName $AppPoolName -Binding $Binding;
+}
+
+# Figure out what the name of the branch is
+$WorkingPath = $(get-location).Path;
+$ws = [Microsoft.TeamFoundation.VersionControl.Client.Workstation]::Current.GetLocalWorkspaceInfoRecursively($WorkingPath);
+$BranchName = $ws.Name;
+if ( [String]::IsNullOrEmpty($BranchName) ) {
+    Write-Host "Branch Name could not be determined. Exiting Script";
+} else {
+    Write-Host "Detected Branch name: " $BranchName;
+    ConfigureLocalSite $BranchName.ToLower();
+    $LoginSite = "login." + $BranchName.ToLower();
+    ConfigureLocalSite $LoginSite;
+    IISRESET;
+}
+
+Write-Host "All Local Configuration Complete. Press any key to close this window.";
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
